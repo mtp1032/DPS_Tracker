cf43fa2a4ae8221a01fbebbf682a4e2e16d97a5f
Michael Peterson <mtpeterson1948@gmail.com>
2023-07-16 13:01:08 -0600

    Update MsgFrame.lua
    Removed unused functions

--- Utilities/MsgFrame.lua
+++ Utilities/MsgFrame.lua
@@ -13,39 +13,19 @@ local SUCCESS	= base.SUCCESS
 local FAILURE	= base.FAILURE
 
 local frameTitle = L["USER_MSG_FRAME"]
-local msgFrame = frames:createMsgFrame( frameTitle)
-local frameTitle = sprintf("%s %s", L["ADDON_AND_VERSION"], L["ERROR_MSG_FRAME_TITLE"])
-local errorMsgFrame = frames:createErrorMsgFrame(frameTitle)
 
-function mf:getMsgFrame()
-	return msgFrame
-end
-function mf:showFrame()
-	frames:showFrame( msgFrame )
-end
-function mf:eraseText() 
-	frames:clearFrame( msgFrame )
-end
-function mf:hideFrame()
-	frames:hideFrame( msgFrame )
-end
+local errorMsgFrame 	= display:createErrorMsgFrame( L["ERROR_MSG_FRAME"])
+local userMsgFrame 		= display:createMsgFrame( L["USER_MSG_FRAME"] )
+local combatMsgFrame	= display:createMsgFrame( L["CLEU_MSG_FRAME"])
 
-function mf:hideMeter()
-	frames:hideMeter( msgFrame )
-end
-function mf:showMeter()
-    if errorMsgFrame == nil then
-        return
-	end
-	if errorMsgFrame:IsVisible() == false then
-		errorMsgFrame:Show()
-	end
-end
 function mf:postMsg( msg )
-	frames:showFrame( msgFrame )
-	msgFrame.Text:Insert( msg )
+	display:showFrame( userMsgFrame )
+	userMsgFrame.Text:Insert( msg )
 end
-
+function mf:postLogEntry( logEntry )
+	display:showFrame( combatMsgFrame )
+	combatMsgFrame.Text:Insert( logEntry )
+end
 function mf:postResult( result )
 	local status = nil
 	if result[1] ~= FAILURE then 
@@ -53,36 +33,13 @@ function mf:postResult( result )
 	end
 	local topLine = sprintf("[%s] %s: %s\n", "FAILURE", result[2], result[3])
 	errorMsgFrame.Text:Insert( topLine )
-	mf:showMeter()
-end
-function mf:clearText()
-	fm:clearFrameText()
+	if errorMsgFrame:IsVisible() == false then
+		errorMsgFrame:Show()
+	end
 end
 
 local fileName = "MsgFrame.lua"
+
 if base:debuggingIsEnabled() then
 	DEFAULT_CHAT_FRAME:AddMessage( sprintf("%s loaded", fileName), 1.0, 1.0, 0.0 )
 end
--- [string "@Interface/AddOns/   DPS_Tracker/LibUtils/MsgFrame.lua"]:64: in function <Interface/AddOns/DPS_Tracker/LibUtils/MsgFrame.lua:63>
--- [string "@Interface/AddOns/   DPS_Tracker/LibUtils/MsgFrame.lua"]:69: in function <Interface/AddOns/DPS_Tracker/LibUtils/MsgFrame.lua:68>
--- [string "@Interface/AddOns/   DPS_Tracker/LibUtils/MsgFrame.lua"]:79: in function `top'
--- [string "@Interface/AddOns/   DPS_Tracker/LibUtils/MsgFrame.lua"]:83: in main chunk
-
--- /dump strsplittable(":", "hello:world:banana")
--- > {
--- 	[1] = "hello",
--- 	[2] = "world",
--- 	[3] = "banana"
--- }
--- local fragments = {}
-
--- local stackTrace = debugstack(2)
--- local splitFragments = strsplittable(">", stackTrace )
--- print( type(splitFragments ), #splitFragments )
--- print( splitFragments[1] )
-
--- local num = #splitFragments
--- for i = 1, num do
--- 	mf:postMsg( sprintf("splits[%d] = %s\n", i, splitFragments[i]))
--- end
-
